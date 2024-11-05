3f8f428c024925b37ebbb8f3009729e04284ac73
SanjeevKhare <90015656+SanjeevKhare@users.noreply.github.com>
2024-11-05 12:13:51 -0500

    Initial commit

---
+++ Circle.js
@@ -0,0 +1,33 @@
+
+function Circle(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.r = 1;
+    this.growing = true;
+    // this.color = color(random(255), random(255), random(255), 180);
+    this.color = color;
+
+    this.grow = function () {
+        if (this.growing) {
+            this.r += 1;
+        }
+    };
+
+    this.show = function () {
+        // stroke(255);
+        // noFill();
+        fill(this.color);
+        // strokeWeight(2);
+        noStroke();
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    };
+
+    this.edges = function () {
+        return (
+            this.x + this.r >= width ||
+            this.x - this.r <= 0 ||
+            this.y + this.r >= height ||
+            this.y - this.r <= 0
+        );
+    };
+}
---
+++ sketch.js
@@ -0,0 +1,146 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/QHEQuoIKgNE
+
+let circles;
+let img;
+positions = [];
+let colorFrequency = {};
+
+function preload() {
+  img = loadImage('assets/gc1.png');
+}
+
+function setup() {
+  // createCanvas(1640, 1160);
+  createCanvas(img.width, img.height);
+  var density = displayDensity();
+  pixelDensity(1);
+  img.loadPixels();
+  circles = [];
+
+  console.log(img.width);
+  console.log(img.height);
+  console.log('pixels', img.pixels.length);
+  console.log(density);
+
+  positions = findPositions(img, 255, 255, 255);
+  console.log('Positions:', positions);
+}
+
+function draw() {
+  background(245, 245, 220);
+
+  // Fill the lower half with cyan color
+  fill(0, 255, 255);
+  noStroke();
+  beginShape();
+  vertex(0, height / 2);
+  for (let x = 0; x <= width; x += 10) {
+    let y = -frameCount * 0.2 + height / 2 + 20 * sin(TWO_PI * x / 10 + frameCount * 0.1);
+    vertex(x, y);
+  }
+  vertex(width, height / 2);
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
+
+  // frameRate(20);
+
+  let total = 6;
+  let count = 0;
+  let attempts = 0;
+
+  while (count < total) {
+    let newC = newCircle();
+    if (newC !== null) {
+      circles.push(newC);
+      count++;
+    }
+    attempts++;
+    if (attempts > 100) {
+      noLoop();
+      console.log('finished');
+      break;
+    }
+  }
+
+  for (let i = 0; i < circles.length; i++) {
+    let circle = circles[i];
+    if (!circle.growing) {
+      circle.show();
+      circle.grow();
+      continue;
+    }
+
+
+    if (circle.edges()) {
+      circle.growing = false;
+    }
+    else {
+      for (let j = 0; j < circles.length; j++) {
+        let other = circles[j];
+        if (circle !== other) {
+          let d = dist(circle.x, circle.y, other.x, other.y);
+          let distance = circle.r + other.r;
+
+          if (d < distance) {
+            circle.growing = false;
+            break;
+          }
+        }
+      }
+    }
+
+    circle.show();
+    circle.grow();
+  }
+}
+
+function newCircle() {
+
+  console.log('positions size', positions.length);
+  if (positions.length === 0) {
+    return null;
+  }
+
+  let pos = random(positions);
+  let x = pos.x;
+  let y = pos.y;
+
+  let valid = true;
+  for (let i = 0; i < circles.length; i++) {
+    let circle = circles[i];
+    let d = dist(x, y, circle.x, circle.y);
+    if (d < circle.r) {
+      console.log('invalid');
+      valid = false;
+      break;
+    }
+  }
+
+  if (valid) {
+    var c = color(random(255), random(255), random(0, 150));
+    console.log('color', c);
+    return new Circle(x, y, c);
+  } else {
+    return null;
+  }
+}
+
+function findPositions(img, r, g, b) {
+  let positions = [];
+  for (let y = 0; y < img.height; y++) {
+    for (let x = 0; x < img.width; x++) {
+      let index = (x + y * img.width) * 4;
+      let red = img.pixels[index];
+      let green = img.pixels[index + 1];
+      let blue = img.pixels[index + 2];
+      if (red === r && green === g && blue === b) {
+        positions.push({ x: x, y: y });
+      }
+    }
+  }
+  return positions;
+}
